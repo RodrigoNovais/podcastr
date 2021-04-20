bbfd0f0db9c350204ed8dc0ffc31970d468e5c2f
Rodrigo Tolentino de Novais <roo.novais@gmail.com>
2021-04-20 11:30:18 -0300

    style: Base style and assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { AppProps } from 'next/app'
+import React from 'react';
+import { AppProps } from 'next/app';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
     <Component {...pageProps} />
-)
+);
 
 export default App;
