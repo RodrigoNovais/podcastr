258451d5cfc5ef068462be569b7f1f8bc434ccfc
Rodrigo Tolentino de Novais <roo.novais@gmail.com>
2021-04-28 17:27:12 -0300

    feat: Set default layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import Header from '../components/Header';
+import Player from '../components/Player';
+
+import styles from '../styles/app.module.css';
+
 import '../styles/globals.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-    <Component {...pageProps} />
+    <div className={styles.appWrapper}>
+        <main>
+            <Header />
+            <Component {...pageProps} />
+        </main>
+
+        <Player />
+    </div>
 );
 
 export default App;
